673dc38df3981577a410444e58d4f31e9fbb5340
elbuo8 <yamil.asusta@upr.edu>
2012-09-27 12:18:27 -0400

    Updated merge sort.

--- finished/mergesort.java
+++ finished/mergesort.java
@@ -15,8 +15,7 @@ public class mergesort {
 
 	static int[] sort(int[] array) {
 		//base case
-		if(array.length <= 1)
-			return array;
+		if(array.length <= 1) return array;
 
 		//divide into sublists
 		int[] left = new int[array.length/2];
@@ -37,24 +36,16 @@ public class mergesort {
 
 	static int[] merge(int[] left, int[] right) {
 		int[] array = new int[left.length + right.length];
+		int leftIndex = 0;
+		int rightIndex = 0;
 		
-		int indexi = 0;
-		int indexj = 0;
-		int key = 0;
-
-		while (indexi < left.length && indexj < right.length) {
-			if(left[indexi] < right[indexj]) 
-				array[key++] = left[indexi++];
-			else
-				array[key++] = right[indexj++];
+		for (int i = 0; i < array.length; i++) {
+			if(leftIndex < left.length && rightIndex < right.length) 
+				array[i] = (left[leftIndex] <= right[rightIndex]) ? left[leftIndex++]:right[rightIndex++];
+			else if(leftIndex >= left.length) array[i] = right[rightIndex++];
+			else array[i] = left[leftIndex++];
 		}
-		if(indexi < left.length)
-			for (int i = indexi; i < left.length; i++) 
-				array[key++] = left[i];
-		
-		if(indexj < right.length)
-			for (int i = indexj; i < right.length; i++) 
-				array[key++] = right[i];
+
 		return array;
 	}
 
