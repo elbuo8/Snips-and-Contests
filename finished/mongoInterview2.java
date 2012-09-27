90e381b09b5c3241828af565f716e110afe62efd
elbuo8 <yamil.asusta@upr.edu>
2012-09-27 14:40:30 -0400

    False flags elimination

--- finished/mongoInterview2.java
+++ finished/mongoInterview2.java
@@ -15,15 +15,37 @@ public class mongoInterview2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String word = "parrot";
-		String[] words = {"parot", "ferret", "rrotpa", "aro", "rortap", "paarroot"};
+		String word = "abc";
+		String[] words = {"bbb", "bca", "rrotpa", "aac", "rortap", "paarroot"};
 		System.out.println(Arrays.toString(findAnagrams(word, words)));
 
 	}
 	
 	static String[] findAnagrams(String word, String[] words) {
 		
+		
+		char[] letters = new char[26];
+		for (int i = 0; i < word.length(); i++) 
+			letters[Math.abs(49-(word.charAt(i) - '0'))] += 1;
+		
 		ArrayList<String> anagrams = new ArrayList<String>();
+		int key = word.length();
+		
+		for (int i = 0; i < words.length; i++) {
+			if(words[i].length() == key) {
+				char[] parse = new char[26];
+				for (int j = 0; j < words[i].length(); j++) 
+					parse[Math.abs(49-(words[i].charAt(j) - '0'))] += 1;
+				boolean flag = true;
+				for (int j = 0; j < parse.length; j++)
+					if(parse[j] != letters[j]) flag = false;
+				if(flag)
+					anagrams.add(words[i]);
+				
+			}
+		}
+		
+		/*
 		int key = word.length();
 		int index = 0;
 		
@@ -43,7 +65,7 @@ public class mongoInterview2 {
 				if(value == index) anagrams.add(words[i]);
 			}
 		}
-		
+		*/
 		return anagrams.toArray(new String[anagrams.size()]);
 	}
 }
